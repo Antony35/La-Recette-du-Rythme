8c209a486245fd67bec7d936c642a8c815648049
Antony <antony.huart37@gmail.com>
2026-07-26 16:59:36 +0200

    chore: config Biome (lint + format)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
-import tailwindcss from '@tailwindcss/vite';
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+	integrations: [vue()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
